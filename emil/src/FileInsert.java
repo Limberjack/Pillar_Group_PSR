e4781a6dd6d1777c28d52319c22c6cd792cd925a
Emil <emil.73@mail.ru>
2019-04-09 17:30:58 +0300

    copy method added

--- emil/src/FileInsert.java
+++ emil/src/FileInsert.java
@@ -1,3 +1,21 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+
+
 public class FileInsert {
-    //LERO LERO LERO LERO LERO LERO LERO LERO LERO LERO LERO LERO LERO LERO LERO LERO LERO LERO LERO LERO LERO LERO LERO LERO LERO LERO LERO LERO LERO LERO
+    public void copyFile(String originFilePath, String destinationFolderPath)
+            throws Exception
+    {
+        File orig = new File(originFilePath);
+
+        if (!orig.exists() || !orig.isFile()) {
+            throw new FileNotFoundException("Origin is invalid");
+        }
+        destinationFolderPath = destinationFolderPath+"\\"+orig.getName();
+        File dest = new File(destinationFolderPath);
+        Files.copy(orig.toPath(),dest.toPath(), StandardCopyOption.REPLACE_EXISTING);
+    }
+
 }
