85111e49a0f33b9b3ff67746ae5b3b0584bba4e1
Emil <emil.73@mail.ru>
2019-04-19 17:38:55 +0300

    Deleted old DB

--- src/database/DBProgramable.java
+++
@@ -1,9 +0,0 @@
-package database;
-
-/**
- * Use this interface to describe the user's program
- */
-public interface DBProgramable {
-    String getProgramName();
-    String getProgramVersion();
-}
